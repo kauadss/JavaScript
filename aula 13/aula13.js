87e5ab7d3e322f3c49c9147a3a2fc1863459253c
kauadss <144458139+kauadss@users.noreply.github.com>
2023-11-14 11:29:28 -0300

    aulas de if e switch

---
+++ aula 13/aula13.js
@@ -0,0 +1,14 @@
+let num=100
+
+if(num > 50){
+    console.log("Numero maior que 50")
+    if(num > 70){
+        console.log("Numero maior que 70")
+    }
+} else if(num>5){
+    console.log("Numeral entre 6 e 10")
+} else{
+    console.log("Numero menor que 5")
+}
+
+console.log("Fim do programa")
---
+++ aula 14/aula14.js
@@ -0,0 +1,19 @@
+let colocação=3
+
+switch(colocação){
+    case 1:
+        console.log("Primeiro lugar")
+        break
+    case 2:
+        console.log("Segundo lugar")
+        break
+    case 3:
+        console.log("Terceiro lugar")
+        break
+    case 4: case 5: case 6:
+        console.log("Premio de participação")
+        break
+    default:
+        console.log("Perdeu")
+        break
+}
