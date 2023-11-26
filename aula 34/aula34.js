e6c386c9149740a695ae6b4988f805fcea1d2a7a
kauadss <144458139+kauadss@users.noreply.github.com>
2023-11-26 11:44:40 -0300

    addEvent

---
+++ aula 34/aula34.js
@@ -0,0 +1,15 @@
+//const c1=document.querySelector("#c1")
+const cursos=[...document.querySelectorAll(".curso")]
+
+cursos.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        const el=evt.target
+        el.classList.add("destaque")
+        console.log(el.innerHTML + " foi clicado")
+    })
+})
+
+//c1.addEventListener("click",msg)
+/*c1.addEventListener("click",()=>{
+    alert("clicou")
+})*/
