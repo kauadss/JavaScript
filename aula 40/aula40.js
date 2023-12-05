4c7cb6ddab619f3d113a881cb15b2732010921c9
kauadss <144458139+kauadss@users.noreply.github.com>
2023-12-05 12:23:33 -0300

    criando elementos

---
+++ aula 40/aula40.js
@@ -0,0 +1,20 @@
+const caixa1=document.querySelector("#caixa1")
+const btn_c=[...document.querySelectorAll(".curso")]
+const c1_2=document.querySelector("#c1_2")
+const cursos=["HTML","CSS","JavaScript","PHP","React","MySQL"]
+
+
+cursos.map((el,chave)=>{
+    const novoElemento=document.createElement("div")
+    novoElemento.setAttribute("id","c"+chave+1)
+    novoElemento.setAttribute("class","curso c1")
+    novoElemento.innerHTML=el
+    caixa1.appendChild(novoElemento)
+})
+
+// const novoElemento=document.createElement("div")
+// novoElemento.setAttribute("id","c7")
+// novoElemento.setAttribute("class","curso c1")
+// novoElemento.innerHTML="ReactNative"
+
+// caixa1.appendChild(novoElemento)
