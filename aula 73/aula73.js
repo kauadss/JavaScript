d237fc91987032b5961d3990a5369be2c766e9a9
kauadss <kauasobral90@gmail.com>
2024-01-02 12:09:54 -0300

    clipboard

---
+++ aula 73/aula73.js
@@ -0,0 +1,68 @@
+const telcasNumericas=[...document.querySelectorAll(".num")] 
+const teclasOp=[...document.querySelectorAll(".op")]
+const teclaRes=document.querySelector(".res")
+const display=document.querySelector(".display")
+const tcpy=document.getElementById("tcpy")
+const limpar=document.getElementById("tcelar")
+const tigual=document.getElementById("tigual")
+const teste=document.getElementById("teste")
+
+
+
+let sinal=false
+let decimal=false
+
+telcasNumericas.forEach((el)=>{
+    el.addEventListener("click",(evt)=>{
+        sinal=false
+        if(evt.target.innerHTML==","){
+           if(!decimal){
+            decimal=true
+            if(display.innerHTML=="0"){
+                display.innerHTML="0,"
+            }else{
+                display.innerHTML+=evt.target.innerHTML
+            }
+           } }else{
+            if(display.innerHTML=="0"){
+                display.innerHTML=""
+            }
+        display.innerHTML+=evt.target.innerHTML
+        }
+    })
+})
+
+teclasOp.forEach((el)=>{
+    el.addEventListener("click",(evt)=>{
+        if(!sinal){
+            sinal=true
+            if(display.innerHTML=="0"){
+                display.innerHTML=""
+            }
+            if(evt.target.innerHTML=="x"){
+                display.innerHTML+="*"
+            }else{
+                display.innerHTML+=evt.target.innerHTML
+            }
+        }
+    })
+})
+
+limpar.addEventListener("click",(evt)=>{
+    sinal=false
+    decimal=false
+    display.innerHTML="0"
+})
+
+tigual.addEventListener("click",(evt)=>{
+    sinal=false
+    decimal=false
+    const res=eval(display.innerHTML)
+    display.innerHTML=res
+})
+
+tcpy.addEventListener("click",(evt)=>{
+    teste.select()
+    teste.setSelectionRange(0,9999)
+    navigator.clipboard.writeText(teste.value)
+})
