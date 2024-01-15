d099ed44e6a149c472cd1e353c213065296cd55b
kauadss <kauasobral90@gmail.com>
2024-01-15 15:45:44 -0300

    aula 85

--- aula 85/aula85.js
+++ aula 85/aula85.js
@@ -27,15 +27,33 @@ class Bola{
         this.desenhar()
         this.controle=setInterval(this.controlar,10)
         this.eu=document.getElementById(this.id)
+        numBola++
+        num_objetos.innerHTML=numBola
     }
 
 MinhaPos=()=>{
+    return this.arrayBolas.indexOF(this)
 }
 
 remover=()=>{
+    clearInterval(this.controle)
+    bolas=bolas.filter((b)=>{
+        if(b.id!=this.id){
+            return b
+        }
+    })
+    this.eu.remove()
+    numBola--
+    num_objetos.innerHTML=numBola
+
 }
 
-desenhar=()=>{  
+desenhar=()=>{
+    const div=document.createElement("div")
+    div.setAttribute("id",this.id)
+    div.setAttributeNode("class","bola")
+    div.setAttribute("style",`left:${this.px};top:${this.py};width:${this.tam};height:${this.tam};background-color:rgb(${this.r},${this.g},${this.b})`)
+    this.palco.appendChild(div)  
 }
 
 controlar=()=>{
