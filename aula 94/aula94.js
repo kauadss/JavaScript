d10e9eabc0a0628d39027d44fb567c2d00b01a84
kauadss <kauasobral90@gmail.com>
2024-01-22 09:56:06 -0300

    Importação nomeada

---
+++ aula 94/aula94.js
@@ -0,0 +1,9 @@
+// import { cursos } from "./teste.js";
+// import { getCurso } from "./teste.js";
+// import getTodosCursos from "./teste.js";
+import * as Exemplo from "./aula94-2.js"
+
+
+console.log(Exemplo.cursos)
+console.log(Exemplo.default())
+console.log(Exemplo.getCurso(2))
