61b5ef182363b8064eb18251956b7612c4ba1d4a
kauadss <kauasobral90@gmail.com>
2024-01-27 11:21:14 -0300

    string

---
+++ aula 101/aula101.js
@@ -0,0 +1,12 @@
+let nome=new String("Kauã Sobral")
+let curso=new String("JavaScript")
+
+console.log(nome.charAt(0))
+console.log(nome.charAt(1))
+
+console.log(nome.charCodeAt(0))
+
+console.log(nome.concat(curso))
+
+console.log(nome.indexOf("o"))
+console.log(nome.lastIndexOf("a"))
