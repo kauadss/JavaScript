3264d17ca3acbe45fbe248eb4dc791a2a323db09
kauadss <kauasobral90@gmail.com>
2024-01-28 09:35:42 -0300

    Mais sobre a classe String

---
+++ aula 102/aula102.js
@@ -0,0 +1,26 @@
+let nome=new String("Kauã Sobral")
+let nome2=new String("Kauã Sobral")
+let curso=new String("JavaScript")
+
+console.log(nome.charAt(0))
+console.log(nome.charAt(1))
+
+console.log(nome.charCodeAt(0))
+
+console.log(nome.concat(curso))
+
+console.log(nome.indexOf("o"))
+console.log(nome.lastIndexOf("a"))
+
+console.log(nome.localeCompare(nome2))
+
+console.log(nome.replace("Kauã","Kaue"))
+
+console.log(nome.search("Sobral"))
+
+let sobrenome=nome.slice(5,11)
+console.log(sobrenome)
+
+let arr_nome=nome.split(" ")
+console.log(arr_nome)
+
