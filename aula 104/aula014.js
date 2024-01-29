a07bdb4400b4578eb202210b956adb1dcd7d7356
kauadss <kauasobral90@gmail.com>
2024-01-29 15:30:57 -0300

    Metodo String

---
+++ aula 104/aula014.js
@@ -0,0 +1,12 @@
+let nome=new String("Kauã da Silva Sobral")
+
+console.log(nome.startsWith("K"))
+
+console.log(nome.endsWith("Sobral"))
+
+console.log(nome.includes("Teste"))
+
+console.log(nome.repeat(4))
+console.log(nome.valueOf())
+
+console.log(String.fromCodePoint(66,114,117,110,111))
