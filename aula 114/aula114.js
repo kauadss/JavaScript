b53bf71b7d0a89434d28947be9b17864fb5b444d
kauadss <kauasobral90@gmail.com>
2024-02-07 12:03:41 -0300

    desestruturação

---
+++ aula 114/aula114.js
@@ -0,0 +1,40 @@
+let a,b,c,d;
+
+[a,b,c,d]=[10,20,30,40];
+
+({cor1,cor2,cor3,cor4}={cor1:"vermelho",cor2:"amarelo",cor3:"rosa",cor4:"roxo"});
+
+let numeros=[50,80,90,60];
+
+let [e,f,g,h]=numeros;
+
+[i,f=0,j=0,k=0]=[70];
+
+let x=100;
+let z=300;
+[x,z]=[z,x];
+
+let numero=()=>{
+    return [500,777,200,300]
+};
+
+let [xx,zz,aa,bb]=numero();
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(cor1)
+console.log(cor2)
+console.log(cor3)
+console.log(cor4)
+console.log(e)
+console.log(f)
+console.log(g)
+console.log(h)
+console.log(i)
+console.log(f)
+console.log(j)
+console.log(k)
+console.log(x)
+console.log(z)
