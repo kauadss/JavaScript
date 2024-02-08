ffcabcf0bb42a9d821783dc4e2a4d0c9e64e2277
kauadss <kauasobral90@gmail.com>
2024-02-08 14:22:40 -0300

    Geolocalização

---
+++ aula 116/aula116.js
@@ -0,0 +1,17 @@
+const long=document.getElementById("long")
+const lati=document.getElementById("lati")
+
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(mostrarLocalização,erroLocalização)
+} else{
+    console.log("Geolocalização não suportada")
+}
+
+function mostrarLocalização(pos){
+    long.innerHTML=`Longitude: ${pos.coords.longitude}`
+    lati.innerHTML=`Latitude: ${pos.coords.latitude}`
+}
+
+function erroLocalização(){
+    console.log("Erro ao obter a localização")
+}
